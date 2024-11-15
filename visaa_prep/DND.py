53da3f32cd2284cda21a75e1decd3acfb52b135b
Manju Bhargavi Mareedu <119513076+Manjubhargavi132@users.noreply.github.com>
2024-11-15 20:46:17 +0530

    Create DND.py

---
+++ visaa_prep/DND.py
@@ -0,0 +1,7 @@
+N, m = map(int, input().split())
+array= list(map(int, input().split()))
+
+num1 = sum(x for x in array if x % m != 0)
+num2 = sum(x for x in array if x % m == 0)
+
+print(num2 - num1)
